dbd7d526eac023769351b7d8ed223698c0eeba22
Raivo.U <118802231+raivoudris@users.noreply.github.com>
2023-11-28 10:33:30 +0200

    Add files via upload

---
+++ TERA_2/sketch_nov21a/Jettson_reciver.cpp
@@ -0,0 +1,12 @@
+#include "Jettson_reciver.h"
+#include "Arduino.h"
+
+Jettson_reciver_data::Jettson_reciver_data()
+{
+
+}
+
+int Jettson_reciver_data::Data_from_jettson_serial()
+{
+
+}
---
+++ TERA_2/sketch_nov21a/Jettson_reciver.h
@@ -0,0 +1,14 @@
+#ifndef Jettson_reciver_h
+#define Jettson_reciver_h
+#include "Arduino.h"
+
+class Jettson_reciver_data
+{
+  public:
+    int Data_from_jettson_serial();
+
+  private:
+
+};
+
+#endif
