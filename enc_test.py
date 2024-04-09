2772d95117ee9649f523920f1b065a2652b57baa
tontan87261 <118802132+tontan87261@users.noreply.github.com>
2024-04-09 12:14:05 +0300

    Update enc_test.py

--- enc_test.py
+++ enc_test.py
@@ -1,40 +1,38 @@
-int cs_pin = 30;
-int data_pin = 32;
-int clock_pin = 28;
+#include "Steering_encoder.h"
+#include "Arduino.h"
+#include <Stepper.h>
 
-void setup() {
-  Serial.begin(9600);
-  pinMode(cs_pin, OUTPUT);
-  pinMode(data_pin, INPUT);
-  pinMode(clock_pin, OUTPUT);
-  /*
-  _cs_pin = cs_pin;
-  _data_pin = data_pin;
-  _clock_pin = clock_pin;
-  */
-
+Steering_encoder_data::Steering_encoder_data(int STEERING_ENCODER_CS_OUT_Pin, int STEERING_ENCODER_CLK_OUT_Pin, int STEERING_ENCODER_DO_IN_Pin) 
+{
+  pinMode(STEERING_ENCODER_CS_OUT_Pin, OUTPUT);
+  pinMode(STEERING_ENCODER_CLK_OUT_Pin, OUTPUT);
+  pinMode(STEERING_ENCODER_DO_IN_Pin, INPUT);
+  _STEERING_ENCODER_CS_OUT_Pin = STEERING_ENCODER_CS_OUT_Pin; // Replace with your pin number
+  _STEERING_ENCODER_CLK_OUT_Pin = STEERING_ENCODER_CLK_OUT_Pin; // Replace with your pin number
+  _STEERING_ENCODER_DO_IN_Pin = STEERING_ENCODER_DO_IN_Pin;  // Replace with your pin number
 }
 
-
-void loop() {
-  int reading = 0;
-  // Pull CS low to select the encoder
-  digitalWrite(cs_pin, LOW);
-  // Read 12 bits from the encoder
-  for (int i = 0; i < 12; i++) {
-    // Toggle the clock
-    digitalWrite(clock_pin, HIGH);
-    delayMicroseconds(1); // Short delay for clock pulse
-    digitalWrite(clock_pin, LOW);
-    // Read a bit of data
-    reading <<= 1;
-    if (digitalRead(data_pin)) {
-      reading |= 1;
+int Steering_encoder_data::readEncoder() 
+{
+  int i;
+  uint8_t Resolution = 12;
+  uint32_t bitstart = 0x0800;
+  uint16_t encoder_reading = 0;
+  digitalWrite(_STEERING_ENCODER_CS_OUT_Pin, LOW);
+  digitalWrite(_STEERING_ENCODER_CLK_OUT_Pin, LOW);
+  for (i = (Resolution - 1); i >= 0; i--) {
+    digitalWrite(_STEERING_ENCODER_CLK_OUT_Pin, HIGH);
+    if (digitalRead(_STEERING_ENCODER_DO_IN_Pin))
+      encoder_reading |= bitstart;
+    digitalWrite(_STEERING_ENCODER_CLK_OUT_Pin, LOW);
+    bitstart = bitstart >> 1;
+    if (i == 0) {
+      digitalWrite(_STEERING_ENCODER_CLK_OUT_Pin, HIGH);
+      if (digitalRead(_STEERING_ENCODER_DO_IN_Pin))
+        encoder_reading |= bitstart;
     }
-    delayMicroseconds(1);
   }
-  // Pull CS high to deselect the encoder
-  digitalWrite(cs_pin, HIGH);
-  
-  Serial.println(reading);
+  digitalWrite(_STEERING_ENCODER_CS_OUT_Pin, HIGH);
+  //Serial.println(encoder_reading);
+  return encoder_reading;
 }
