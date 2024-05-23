e3cbff94d55df0db59e7a0ceb4ea4eb59d43f26c
EJVirkus <emiljoosep@gmail.com>
2024-05-23 11:15:37 +0300

    wayfinder loogika parandus

--- TERA/Jetson/teleop/teleop/wayfinder.py
+++ TERA/Jetson/teleop/teleop/wayfinder.py
@@ -1,16 +1,16 @@
 from gps import get_gps_data
 import math
 import pandas as pd
-from numpy import genfromtxt
+#from numpy import genfromtxt
 import numpy as np
 
 data = pd.read_csv(r'/home/ejvirkus/TERA/TERA/Jetson/GPS_utils/gps_ENU.csv', header=0, usecols=['X', 'Y'])
 
-data = np.array()
+data_array = np.array(data)
 
 i = 0
 
-def get_target_location(data_array, target_location, min_dist): # Acquiring the closest or next point to which to move
+def get_target_location(): # Acquiring the closest or next point to which to move
     if target_location == []: # If there is no target location, the program finds the closest point to the robot.
         for x in data_array.index:
             current_min_dist = math.dist(data.at[x], get_gps_data()[0, 1]) # Iterating through all points
